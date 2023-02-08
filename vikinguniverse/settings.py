481ecaae19c1e94374df3633f6af71a25c3c63e6
William Crumb <william.crumb@vikingassetmanagement.com>
2023-02-08 10:03:38 +0800

    for test environment

--- vikinguniverse/settings.py
+++ vikinguniverse/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'vikinguniverse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'VikingAssetManag$viking_universe_test',
+        'USER': 'VikingAssetManag',
+        'PASSWORD': 'P4ssw0rd12345$',
+        'HOST': 'VikingAssetManagement.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
